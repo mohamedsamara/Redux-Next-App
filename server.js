365db4956e85ae452bc2e29ad808ee70a45e5985
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-10-13 22:32:28 -0700

    setup mongodb

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const next = require("next");
+const mongoose = require("mongoose");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
+const mongoURI = process.env.MONGO_URI;
+
+// Connect to MongoDB
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch(err => console.log(err));
 
 const app = next({ dev });
 const handle = app.getRequestHandler();
